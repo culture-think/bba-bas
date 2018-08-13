d9443f20f7bd1cfce656ef4bdc4f650f8145677b
张华锋 <phoenicsbasil@hotmail.com>
2018-08-13 10:56:01 +0800

    重构代码

---
+++ bas-admin/src/main/java/com/flynet/bas/controller/mobile/VehicleMobileController.java
@@ -0,0 +1,28 @@
+/* <p>文件名称: VehicleMobileController.java </p>
+ * <p>文件描述: 无</p>
+ * <p>版权所有: 版权所有(C)2017-2027</p>
+ * <p>公    司: 沈阳网飞科技有限公司</p>
+ * <p>内容摘要: 无</p>
+ * <p>其他说明: 无</p>
+ * <p>创建日期：2018年8月13日</p>
+ * <p>完成日期：2018年8月13日</p>
+ * <p>修改记录1: // 修改历史记录，包括修改日期、修改者及修改内容</p>
+ * <pre>
+ *    修改日期：上午10:54:47
+ *    版 本 号：
+ *    修 改 人：
+ *    修改内容：
+ * </pre>
+ * <p>修改记录2：…</p>
+ * @version 1.0
+ * @author	zhanghuafeng
+ */
+package com.flynet.bas.controller.mobile;
+
+/**
+ *
+ * @author zhanghuafeng
+ */
+public class VehicleMobileController {
+
+}
