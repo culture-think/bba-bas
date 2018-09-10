b4ce821fc92fb037d91defc9999a06ad7485df49
culture-think <phoenicsbasil@hotmail.com>
2018-09-10 11:06:48 +0800

    提交车辆选择移动端web控制器

---
+++ bas-admin/src/main/java/com/flynet/bas/controller/mobile/VehicleSelectionMobileController.java
@@ -0,0 +1,33 @@
+/* <p>文件名称: VehicleSelectionMobileController.java </p>
+ * <p>文件描述: 无</p>
+ * <p>版权所有: 版权所有(C)2017-2027</p>
+ * <p>公    司: 沈阳网飞科技有限公司</p>
+ * <p>内容摘要: 无</p>
+ * <p>其他说明: 无</p>
+ * <p>创建日期：2018年9月10日</p>
+ * <p>完成日期：2018年9月10日</p>
+ * <p>修改记录1: // 修改历史记录，包括修改日期、修改者及修改内容</p>
+ * <pre>
+ *    修改日期：上午9:14:46
+ *    版 本 号：
+ *    修 改 人：
+ *    修改内容：
+ * </pre>
+ * <p>修改记录2：…</p>
+ * @version 1.0
+ * @author	zhanghuafeng
+ */
+package com.flynet.bas.controller.mobile;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author zhanghuafeng
+ */
+@RestController
+@RequestMapping(value = "/mobile-api")
+public class VehicleSelectionMobileController {
+
+}
